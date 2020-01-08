17fb416e38086076c3da24de76c6792e040e88e6
Arslan Faisal <programdev97@gmail.com>
2020-01-08 16:38:48 +0500

    first commit

---
+++ build/precache-manifest.1bfa41fbb8bb4046020afe1d3503a400.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cc11a513053cdaa04be0a1e2fbc3fbb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef5c0ab0b6bfa6b72645",
+    "url": "/static/js/2.c054c3ce.chunk.js"
+  },
+  {
+    "revision": "b5bb055fce54e517baee1778d1a2412c",
+    "url": "/static/js/2.c054c3ce.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c2c9b76a9b6165f7f68a",
+    "url": "/static/js/main.158717d0.chunk.js"
+  },
+  {
+    "revision": "748477eeda949924e4d9",
+    "url": "/static/js/runtime-main.d4a7be2e.js"
+  }
+]);
